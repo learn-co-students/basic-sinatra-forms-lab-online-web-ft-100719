b7fd97540cbcebba164cef296aeaf697c312f9b5
markenright <enright.mark@gmail.com>
2019-12-16 11:50:50 -0500

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,20 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+    
+  get '/' do
+    "Hello World"
+  end
+
+  get '/newteam' do
+    erb :newteam
+  end
+
+  post '/team' do
+    @team_data = params
+
+    erb :team
+  end
+
 
 end
